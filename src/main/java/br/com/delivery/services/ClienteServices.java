a9ddc9827b80bfc4b54e20c1fa200cfdf79ab3ce
Christian Felipe Carvalho <64433548+christianfelipecarvalho@users.noreply.github.com>
2023-01-31 21:12:28 -0300

    Revert "Alterações 31/01/2023"

--- src/main/java/br/com/delivery/services/ClienteServices.java
+++ src/main/java/br/com/delivery/services/ClienteServices.java
@@ -1,6 +1,5 @@
 package br.com.delivery.services;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Logger;
 
@@ -9,9 +8,8 @@ import org.springframework.boot.context.config.ConfigDataResourceNotFoundExcepti
 import org.springframework.stereotype.Service;
 
 import br.com.delivery.exceptions.ResourceNotFoundException;
-import br.com.delivery.mocks.ClienteMock;
 import br.com.delivery.model.Cliente;
-//import br.com.delivery.repository.ClienteRepository;
+import br.com.delivery.repository.ClienteRepository;
 
 @Service
 public class ClienteServices {
@@ -19,88 +17,45 @@ public class ClienteServices {
 	private Logger logger = Logger.getLogger(ClienteServices.class.getName());
 	
 	
-	//@Autowired
-	//ClienteRepository repository;
+	@Autowired
+	ClienteRepository repository;
 	public List<Cliente> findAll(){
-		List<Cliente> clientes = new ArrayList<>();
 		
-		for (int i = 0; i < 4; i++) {
-			clientes.add(ClienteMock.criarMockdoCliente(Long.valueOf(i)));
-		}
 		logger.info("TOdos os clientes");
 		
-		//return repository.findAll();
-		return clientes;
+		return repository.findAll();
 	}
 	
 	public Cliente findById(Long id) {
-		Cliente cliente = ClienteMock.criarMockdoCliente(id);
+		
 		logger.info("Encontre um cliente!");
 		
-		return cliente;
-		//return repository.findById(id)
-				//.orElseThrow(() -> new ResourceNotFoundException("Nenhum cliente encontrado com esse Id!"));
-	}
-	
-	
-	public List<Cliente> findByNomeAndSobrenome(String nome, String sobrenome) {
-		List<Cliente> clientes = new ArrayList<>();
-		
-		Cliente cliente1 = ClienteMock.criarMockdoCliente(1L);
-		cliente1.setNome("jorge");
-		cliente1.setSobrenome("costa");
-		
-		Cliente cliente2 = ClienteMock.criarMockdoCliente(2L);
-		cliente2.setNome("christian");
-		cliente2.setSobrenome("felipe");
-		
-		
-		Cliente cliente3 = ClienteMock.criarMockdoCliente(3L);
-		cliente3.setNome(nome);
-		cliente3.setSobrenome(sobrenome);
-		
-		if (cliente1.getNome().equals(nome) && cliente1.getSobrenome().equals(sobrenome)) {
-			clientes.add(cliente1);}
-		
-		
-		if (cliente2.getNome().equals(nome) && cliente2.getSobrenome().equals(sobrenome)) {
-			clientes.add(cliente2);}
-		
-		if (cliente3.getNome().equals(nome) && cliente3.getSobrenome().equals(sobrenome)) {
-			clientes.add(cliente3);}
-		
-		return clientes;
+		return repository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Nenhum cliente encontrado com esse Id!"));
 	}
 	
 	public Cliente create(Cliente cliente) {
 		
 		logger.info("Um cliente criado!");
 		
-		//return repository.save(cliente);
-		return cliente;
-		
+		return repository.save(cliente);
 	}
 	
-	public Cliente update(Cliente cliente) {
+	public Cliente Update(Cliente cliente) {
 		
 		logger.info("Atualizando um cliente!");
 		
-		/*var entity = repository.findById(cliente.getId())
+		var entity = repository.findById(cliente.getId())
 				.orElseThrow(() -> new ResourceNotFoundException("Nenhum registro encontrado com esse ID!"));
 		
 		entity.setNome(cliente.getNome());
 		entity.setSobrenome(cliente.getSobrenome());
 		entity.setCpf(cliente.getCpf());
 		entity.setRg(cliente.getRg());
-		entity.setEmail(cliente.getEmail());*/
+		entity.setEmail(cliente.getEmail());
 		//entity.setEndereco(cliente.getEndereco());
 		
 		
-		//return repository.save(cliente);
-		return cliente;
-	}
-	
-	public void delete(Long id) {
-		
+		return repository.save(cliente);
 	}
 }
